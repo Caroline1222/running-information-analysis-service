971bc9d822a75d0b0d3b08b65f0a827939f809ef
Caroline1222 <congw1222@gmail.com>
2018-01-01 16:34:52 -0500

    First commit

--- src/main/java/demo/domain/RunningInformationRepository.java
+++ src/main/java/demo/domain/RunningInformationRepository.java
@@ -4,11 +4,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 // extends JpaRepository, has two args, entity type and id type
+@RepositoryRestResource(path = "runninginfo")
 public interface RunningInformationRepository extends JpaRepository<RunningInformation, Long> {
     // Pageable is paging parameter
+    @RestResource(path = "runningid")
     RunningInformation findByRunningId(@Param("runningId") String runningId);
+    @RestResource(path = "username")
     Page<RunningInformation> findByUserInfoUsernameOrderByHealthWarningLevelDesc(@Param("username") String username, Pageable pageable);
 
 
